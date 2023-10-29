557c29778cf98b8d26c05d13f7de2b5b1fc2afa8
LuisCQueiroz <djlc2@yahoo.com.br>
2023-10-29 20:12:45 -0300

    Aula de inicio

---
+++ Estrutura de Software e ambinete de desenvolvimento/script.js
@@ -0,0 +1,8 @@
+console.log ("Digite o nome do jogador")
+
+//alert("Hello World")
+
+let nickname = "João caçador de Pokemon"
+console.log ("Bem vindo: " + nickname)
+console.log (nickname + " se logou no servidor corretamente.")
+
