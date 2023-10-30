23367cf130fdb88e0168a734db71f2357eb25732
LuisCQueiroz <djlc2@yahoo.com.br>
2023-10-29 23:26:07 -0300

    Tipos de variáveis
    Tipos de variáveis Boolean, string e number

---
+++ Estrutura de Software e ambinete de desenvolvimento/Tipos de dados de Variáveis/script.js
@@ -0,0 +1,14 @@
+//pokemon
+ 
+ // Tipos de Variaveis
+ 
+ //String servem para armazenar textos
+let nomePokemon = "Pikachu"
+let pokemonSexo = "M"
+
+//Number servem para armazenar números
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+//Boolean servem para armazenar true ou false, ou seja variáveis lógicas
+let Selecionavel = "true"
