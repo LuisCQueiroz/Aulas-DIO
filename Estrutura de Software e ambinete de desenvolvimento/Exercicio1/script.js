e39c49b1e7a0a808e939d2030150f75ccc6d1697
LuisCQueiroz <djlc2@yahoo.com.br>
2023-10-30 14:48:02 -0300

    Exercícios
    Exercícios realizados

---
+++ Estrutura de Software e ambinete de desenvolvimento/Exercicio1/script.js
@@ -0,0 +1,17 @@
+//nome, idade, numero de telefone, cpf, endereço, recebe pensão
+//string para armazenar texto
+//number para aarmazenar numeros
+//boolean armazenar valores logicos
+
+let nomePessoa = "Josicreide do Pythinho"
+let idade = 115
+let numeroTelefone = "(11) 98471-3388"
+let cpf = "312.122.312-45"
+let endereco = "Rua dos Limoeiros, 75, bairro da Jaguatirica"
+let recebePensao = true
+
+document.writeln("<br>O nome do cliente é: " + nomePessoa + "<br>")
+document.writeln("<br>A idade do cliente é: " + idade + " anos.<br>" )
+document.writeln("<br>O número do telefone é: " + numeroTelefone + "<br>" )
+document.writeln("<br>O endereço de entrega é: " + endereco + "<br>" )
+
