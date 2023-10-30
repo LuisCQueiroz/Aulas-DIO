88005535425b0726f73755d8fd7b0696224a8585
LuisCQueiroz <djlc2@yahoo.com.br>
2023-10-29 23:03:12 -0300

    Exemplos de atualização de variáveis no JavaScript
    Exemplos de atualização de variáveis no JavaScript

---
+++ Estrutura de Software e ambinete de desenvolvimento/Variaveis/script.js
@@ -0,0 +1,18 @@
+let poteCafe = "Café Pilão"
+let poteAcucar = "Açucar Cristal"
+let poteBiscoito = "Biscoito Maizena"
+const mensagemDaVovo = "Na cozinha da vovó hoje tem: "
+
+console.log (mensagemDaVovo + 
+             poteCafe + " - " + 
+             poteAcucar + " - " +
+			 poteBiscoito
+			 )
+
+poteCafe = "Café 3 Corações"
+
+console.log (mensagemDaVovo + 
+             poteCafe + " - " + 
+             poteAcucar + " - " +
+			 poteBiscoito
+			 )
