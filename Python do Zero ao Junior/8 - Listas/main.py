955c88ef6e368c6263f4ce047e70e6e06c65bfa6
LuisCQueiroz <djlc2@yahoo.com.br>
2024-02-17 20:46:43 -0300

    Continuação das aulas Python
    Continuação das aulas Python

---
+++ Python do Zero ao Junior/8 - Listas/main.py
@@ -0,0 +1,22 @@
+lista_numeros = [1, 2, 3]
+
+print("Lista 1")
+print(lista_numeros)
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+print()
+
+lista_numeros1 = [3, 4, 5]
+
+print("Lista 2")
+print(lista_numeros1)
+print(lista_numeros1[0])
+print(lista_numeros1[1])
+print(lista_numeros1[2])
+
+lista_numeros1[0] = 5
+print("Alterando a posição 1 da lista 2 (lista_numeros1[0]) para 5")
+print(lista_numeros1[0])
+print(lista_numeros1[1])
+print(lista_numeros1[2])
